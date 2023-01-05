416098229206b88ac8eacb586b9b9575d2fef793
Dmitriy <68978608+reewardius@users.noreply.github.com>
2023-01-05 22:40:46 +0200

    Update eval.php

--- Code Injection/eval.php
+++ Code Injection/eval.php
@@ -12,3 +12,8 @@ $empty = 'No variable given';
 eval('echo $' . $variable . ';');
 
 "';system('cat /etc/passwd');//" --> echo $';system('cat /etc/passwd');//;
+
+In general, any function that allows the execution of arbitrary code provided as a string can be potentially unsafe to use in PHP. 
+This includes functions like eval, exec, system, and passthru. 
+These functions should be used with caution, if at all, and only with input that has 
+been thoroughly validated to ensure that it does not contain any malicious code.

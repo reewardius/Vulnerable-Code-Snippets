ce0813ea55b27d9ec4d0f353e1623e3b39f2e5bc
Dmitriy <68978608+reewardius@users.noreply.github.com>
2023-01-05 22:25:27 +0200

    Update example1.c

--- Connection String Injection/example1.c
+++ Connection String Injection/example1.c
@@ -3,3 +3,5 @@ int main(int argc, char *argv[])
 	rc = SQLConnect(Example.ConHandle, argv[0], SQL_NTS,
 	(SQLCHAR *) "", SQL_NTS, (SQLCHAR *) "", SQL_NTS);
 }
+
+"';DROP TABLE users;--", the resulting connection string would look like this: "';DROP TABLE users;--";"";""
